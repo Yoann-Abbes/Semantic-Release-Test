632a5a4431f6a8aa02d35a8981d215e28c183c73
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2022-05-06 15:09:08 +0200

    feat: Add feature (#18)
    * refactor: naming
    
    * one commit

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const { add } = require('.')
 const { sub } = require('.')
 const { mul } = require('.')
+const { div } = require('.')
 
 test('add', () => {
   expect(add(1, 2)).toEqual(3)
@@ -12,4 +13,8 @@ test('sub', () => {
 
 test('mul', () => {
   expect(mul(2, 2)).toEqual(4)
+})
+
+test('div', () => {
+  expect(div(2, 2)).toEqual(1)
 })
